6e70a7187e72be0c72b4d7dce370f4629d17c5eb
jash <jash@schulterklopfer-productions.de>
2017-12-18 00:52:30 +0100

    added second voice download script

---
+++ buildBIP39Voice.js
@@ -0,0 +1,28 @@
+
+const async = require('async');
+const tts = require('./lib/tts.js');
+const _ = require('lodash');
+const fs = require('fs');
+
+var index = {};
+
+const c = fs.readFileSync( 'language/en/en_bip39_wordlist.txt' );
+
+const texts = c.toString().split('\n');
+
+async.eachLimit( texts, 10, function( text, nextEach ) {
+  text = text.toLowerCase();
+  tts.get( text, 'tts/en', function(err,r) {
+    if( err ) return nextEach(err);
+    index[r.text] = r.digest;
+    console.log( r.text,'->',r.digest);
+    nextEach();
+  })
+}, function(err) {
+  const fileName = 'tts/en/bip39.json';
+  fs.writeFileSync( fileName, JSON.stringify(index,null,4) );
+  console.log( 'done' );
+})
+
+
+
